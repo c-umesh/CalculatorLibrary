ad3c28ad75d2b7d48b1d5752fce13c5964e66d64
c-umesh <umesh.c@gmail.com>
2020-08-20 14:40:16 +0530

    files changed

--- calculator.py
+++ calculator.py
@@ -3,7 +3,9 @@ Calculator library containing basic math operations.
 """
 
 def add(first_term, second_term):
+    """ add two numbers"""
     return first_term + second_term
 
 def subtract(first_term, second_term):
+    """ substracts two numbers"""
     return first_term - second_term
