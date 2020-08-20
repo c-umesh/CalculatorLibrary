8ad74cf0fde2bb0baa39f49c693de969e11948f0
c-umesh <umesh.c@gmail.com>
2020-08-20 15:47:13 +0530

    whitespaces corrected in calculater.py

--- calculator.py
+++ calculator.py
@@ -2,13 +2,17 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     """ add two numbers"""
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     """ substracts two numbers"""
     return first_term - second_term
-    
+
+
 def multiply(first_term, second_term):
+    """ multiply tww numbers"""
     return first_term * second_term
